b85c5e92a27cec29aa2ce307d349eec8e18a369a
bigmuzzle-sudo <bigmuzzle@gmail.com>
2026-05-26 18:14:44 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,3 +1,4 @@
+```js id="1b8b9k"
 export default async function handler(req, res) {
   // CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -82,3 +83,4 @@ export default async function handler(req, res) {
     });
   }
 }
+```

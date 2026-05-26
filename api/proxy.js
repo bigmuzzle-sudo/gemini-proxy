d601be901d8088e72591d0a00b0f4afa62f62db3
bigmuzzle-sudo <bigmuzzle@gmail.com>
2026-05-26 12:59:59 +0200

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
   try {
-    const url = new URL(req.url, "https://generativelanguage.googleapis.com");
+    const baseUrl = "https://generativelanguage.googleapis.com";
 
-    const target = "https://generativelanguage.googleapis.com" + url.pathname.replace("/api/proxy", "") + url.search;
+    const url = new URL(req.url, "http://localhost");
+    const targetUrl = baseUrl + url.pathname.replace("/api/proxy", "") + url.search;
 
     const headers = new Headers(req.headers);
     headers.delete("host");
@@ -16,20 +17,22 @@ export default async function handler(req, res) {
       options.body = req;
     }
 
-    const response = await fetch(target, options);
+    const response = await fetch(targetUrl, options);
 
     res.status(response.status);
 
-    response.headers.forEach((v, k) => {
-      res.setHeader(k, v);
+    response.headers.forEach((value, key) => {
+      res.setHeader(key, value);
     });
 
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "*");
 
-    const data = Buffer.from(await response.arrayBuffer());
-    res.send(data);
+    const buffer = Buffer.from(await response.arrayBuffer());
+    res.send(buffer);
 
-  } catch (e) {
-    res.status(500).json({ error: e.message });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
 }
